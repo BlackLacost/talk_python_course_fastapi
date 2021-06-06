70393da84fb4e0dfe1488a72fd32e618e4ec4e44
Ilya Lisin <blacklacost@gmail.com>
2021-06-06 13:02:54 +0300

    Добавил заготовленные шаблоны jinja2

--- api/main.py
+++ api/main.py
@@ -1,13 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 from uvicorn.config import LOG_LEVELS
 
 api = FastAPI()
+templates = Jinja2Templates(directory="api/templates")
 
 
 @api.get("/")
-def index():
-    return "Hello Wather app!"
+def index(request: Request):
+    return templates.TemplateResponse(
+        "home/index.html.j2", {"request": request}
+    )
 
 
 if __name__ == "__main__":
