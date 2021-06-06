c3b48a481a91e5ba10d62924afb78e40a628b6aa
Ilya Lisin <blacklacost@gmail.com>
2021-06-06 13:15:07 +0300

    Добавил поддержку статики

--- api/main.py
+++ api/main.py
@@ -1,11 +1,13 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, responses
 from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from uvicorn.config import LOG_LEVELS
 
 api = FastAPI()
 templates = Jinja2Templates(directory="api/templates")
+api.mount("/static", StaticFiles(directory="api/static"), name="static")
 
 
 @api.get("/")
@@ -13,6 +15,11 @@ def index(request: Request):
     return templates.TemplateResponse(
         "home/index.html.j2", {"request": request}
     )
+
+
+@api.get("/favicon.ico")
+def favicon():
+    return responses.RedirectResponse(url="/static/img/favicon.ico")
 
 
 if __name__ == "__main__":
