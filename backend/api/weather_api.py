5cdb1aef544e9983b4b8caa1982c6d684c1d80ba
Ilya Lisin <blacklacost@gmail.com>
2021-06-06 14:01:55 +0300

    Валдивация с помощью pydantic

--- backend/api/weather_api.py
+++ backend/api/weather_api.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter
+from typing import Optional
+
+from fastapi import APIRouter, Depends
+
+from models.location import Location
 
 router = APIRouter()
 
 
-@router.get("/api/weather")
-def weather():
-    return "Some report"
+@router.get("/api/weather/{city}")
+def weather(
+    loc: Location = Depends(),
+    units: Optional[str] = "metric",
+):
+    return f"{loc.city}, {loc.state}, {loc.counter}, {units}"
